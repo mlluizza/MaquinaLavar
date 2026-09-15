4ab5beeada81043de9d7c1fcbe02da335bf73e20
mlluizza <ml9634100@gmail.com>
2026-09-15 16:15:38 -0300

    Máquina Ligada

--- src/Estado.java
+++ src/Estado.java
@@ -1,13 +1,8 @@
 public class Estado {
-    public static final Estado DESLIGADA = ;
-    public static final Estado LIGADA = ;
-
     public static void main(String[] args) {
         Maquina maquina = new Maquina();
 
         maquina.ligar();
         maquina.ligar();
-        maquina.desligar();
-        maquina.desligar();
     }
 }
