68046151a3e5551ec4ea1d1bf5db9bcfeca7374c
mlluizza <ml9634100@gmail.com>
2026-09-15 15:07:04 -0300

    Primeiro commit

---
+++ src/Maquina.java
@@ -0,0 +1,8 @@
+public class Maquina {
+    private boolean maquinaLigada;
+    private boolean maquinaDesligada;
+    private Estado estado;
+    private boolean iniciarLavagem;
+    private boolean tampaFechada;
+
+}
