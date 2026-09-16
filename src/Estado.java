9ed7217cfa29f2c8547109abada6643f91d75be9
Maria Luiza Silva e Silva <ml9634100@gmail.com>
2026-09-15 21:23:34 -0300

    Refactor washing machine operations in Estado.java

--- src/Estado.java
+++ src/Estado.java
@@ -9,5 +9,22 @@ public class Estado {
         maquina.pausarLavagem();
         maquina.abrirTampa();
         maquina.desligar();
+        maquina.ligar();
+        maquina.fecharTampa();
+        maquina.lavar();
+        maquina.iniciarCentrifugacao();
+        maquina.concluirLavagem();
+        maquina.iniciarCentrifugacao();
+        maquina.lavar();
+        maquina.ligar();
+        maquina.lavar(); 
+        maquina.fecharTampa();
+        maquina.lavar(); 
+        maquina.pausarLavagem();
+        maquina.abrirTampa(); 
+        maquina.fecharTampa();
+        maquina.retomarLavagem();
+        maquina.iniciarCentrifugacao(); 
+        
     }
 }
